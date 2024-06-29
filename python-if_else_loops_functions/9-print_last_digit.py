10f44c41bc5ba022b418249f52c159ee668821e9
root <nsedidier@gmail.com>
2024-06-29 11:51:13 -0700

    10th

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@ def print_last_digit(number):
     # Ensure the number is positive
     number = abs(number)
     # Get the last digit
-    last_digit = number % 10
+    last_digit = number[:-1]
     # Print the last digit
     return last_digit
