7f71791480794d6541e2e0f03cb3100001b89dc1
root <nsedidier@gmail.com>
2024-06-29 11:15:59 -0700

    8th

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def islower(c):
+    """Check for lowercase characters."""
+    if 97 <= ord(c) <= 122:
+        return True
+    else:
+        return False
+
+# Example usage:
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
+
