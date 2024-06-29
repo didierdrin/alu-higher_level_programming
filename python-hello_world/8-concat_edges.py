06db10e9d6a56bb1472bc2e52ebfea627b87b07a
root <nsedidier@gmail.com>
2024-06-29 04:56:02 -0700

    9th 3rd

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str.split("programming")[0][39:]+"programming with Python")
+str2 = str.split("programming")[0][39:]+"programming with Python"
+print(str2)
