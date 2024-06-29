2059375ec1cebdbe3555269646fe05dd640e70b2
root <nsedidier@gmail.com>
2024-06-29 11:37:47 -0700

    9th

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-
   for char in str:
     # Convert character to uppercase using ASCII difference
     new_char = chr(ord(char) - 32) if 97 <= ord(char) <= 122 else char
     print("{0}".format(new_char), end="")  # Print without newline initially
-
   print()
