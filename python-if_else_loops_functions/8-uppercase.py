b737e832e9dbf471ca75526bf4a2013c3691a302
root <nsedidier@gmail.com>
2024-06-29 11:31:22 -0700

    9th commit

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ def uppercase(str):
   for char in str:
     # Convert character to uppercase using ASCII difference
     new_char = chr(ord(char) - 32 if ord(char) < 91 else ord(char))
-    print(new_char, end="")  # Print without newline initially
+    print("{0}".format(new_char), end="")  # Print without newline initially
 
   print()
